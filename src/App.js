590f1f2ac5df7a2708e27bb559c10cf799b72272
platosystechnologies <platosysteam@gmail.com>
2022-07-08 20:04:56 +0530

    Upload-image-to-server-Frontend-React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [img, setImg] = useState()
+  const [PreviewImg, setPreviewImage] = useState(null)
+
+
+  const handleChange=(event)=>{
+    console.log("Event:",event)
+    setImg(event.target.files[0]);
+    setPreviewImage(URL.createObjectURL(event.target.files[0]))
+  }
+
+
+  const submit = () => {
+    const formdata = new FormData();
+    formdata.append('file',img);
+    formdata.append('data', { name: 'Priya' });
+
+    console.log("formdata",formdata);
+
+    axios.post('http://localhost:5001/single_img_upload',formdata,{
+      headers:{ "Content-Type":"multipart/form-data"}
+    }).then((res) => {
+      console.log(res);
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container" >
+      <input type="file" className="form-control" name="upload_file" onChange={handleChange}/>
+      <button type="button" onClick={submit}>UPLOAD</button>
+      {/* {PreviewImg !== null ? */}
+      <div>
+          <img class="previewimg" src={PreviewImg} alt="UploadImage" />
+                {/* : null} */}
+                </div>
+      </div>
     </div>
   );
 }
